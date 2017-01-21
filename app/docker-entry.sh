eb1a8ed78a2c0eb85398177466f0233256c5c98a
leopku <leopku@qq.com>
2017-01-21 13:29:44 +0800

    only mount /etc/nginx/nginx.conf and /etc/nginx/conf.d/discourse.conf

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -3,19 +3,21 @@
 cd /var/www/discourse
 mkdir -p tmp/pids
 
-if [ ! -f "/var/www/discourse/.assets_precompiled" ]; then
-    echo "start compiling assets"
-    bundle exec rake assets:precompile 1>/var/www/discourse/.assets_precompiled 2>&1
-    echo "finish compiling assets"
-fi
-
 if [ ! -f "/var/www/discourse/.db_migrated" ]; then
     echo "start db migrating"
     dockerize -wait tcp://${DISCOURSE_DB_HOST}:${DISCOURSE_DB_PORT} bundle exec rake db:migrate 1>/var/www/discourse/.db_migrated 2>&1
     echo "finish db migrating"
 fi
 
+if [ ! -f "/var/www/discourse/.assets_precompiled" ]; then
+    echo "start compiling assets"
+    bundle exec rake assets:precompile 1>/var/www/discourse/.assets_precompiled 2>&1
+    echo "finish compiling assets"
+fi
+
 dockerize -wait tcp://${DISCOURSE_DB_HOST}:${DISCOURSE_DB_PORT} \
     -stdout /var/www/discourse/log/unicorn.stdout.log \
     -stderr /var/www/discourse/log/unicorn.stderr.log \
     bundle exec unicorn -E production -c config/unicorn.conf.rb
+
+dockerize -wait tcp://127.0.0.1:${UNICORN_PORT} /usr/sbin/nginx
